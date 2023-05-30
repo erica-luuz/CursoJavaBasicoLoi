b31e8d1ede2c89cb7aaacc503e224116ac5b9e38
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-05-29 23:22:46 -0300

    Correção aula15 exercicio 1,2,3

---
+++ Eclipse Workspace/cursoJavaBasico/src/curso/java/aula15/labs/exer27.java
@@ -0,0 +1,15 @@
+package curso.java.aula15.labs;
+
+import java.util.Scanner;
+
+public class exer27 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+
+	}
+
+}
